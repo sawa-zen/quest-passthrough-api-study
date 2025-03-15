5b7b87bfb963f76faa0a3a682c40c631aefc06e5
sawa-zen <sawadasuiren@gmail.com>
2025-03-15 23:58:51 +0900

    fixup! first

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Canvas } from '@react-three/fiber'
+import { XR, createXRStore } from '@react-three/xr'
 import './App.css'
 
+const store = createXRStore()
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="root">
+      <button
+        className="enter-ar-button"
+        onClick={() => store.enterAR()}
+      >
+        Enter AR
+      </button>
+      <Canvas>
+        <XR store={store}>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+          <mesh>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={'orange'} />
+          </mesh>
+        </XR>
+      </Canvas>
+    </div>
   )
 }
 
